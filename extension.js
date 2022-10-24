0a0d9192c5c895dd9d2344788aa82cf109620d24
Головин Алексей Валерьевич <golovin_av@rtsoft.ru>
2022-10-24 11:03:15 +0300

    First sketchs of multi-device version.

--- extension.js
+++ extension.js
@@ -17,11 +17,11 @@ let prevBatteryLevel;
 let lastEstimation;
 
 function getCurrentFile() {
-    let stack = (new Error()).stack;
+    var stack = (new Error()).stack;
 
     // Assuming we're importing this directly from an extension (and we shouldn't
     // ever not be), its UUID should be directly in the path here.
-    let stackLine = stack.split('\n')[1];
+    var stackLine = stack.split('\n')[1];
     if (!stackLine)
         throw new Error('Could not find current file');
 
@@ -31,19 +31,19 @@ function getCurrentFile() {
     // In the case that we're importing from
     // module scope, the first field is blank:
     //   @/home/user/data/gnome-shell/extensions/u@u.id/prefs.js:8
-    let match = new RegExp('@(.+):\\d+').exec(stackLine);
+    var match = new RegExp('@(.+):\\d+').exec(stackLine);
     if (!match)
         throw new Error('Could not find current file');
 
-    let path = match[1];
-    let file = Gio.File.new_for_path(path);
+    var path = match[1];
+    var file = Gio.File.new_for_path(path);
     return [file.get_path(), file.get_parent().get_path(), file.get_basename()];
 }
 
 function init () {
     // get the contents of the json file
-    let [, extensionPath,] = getCurrentFile();
-    let [ok, contents] = GLib.file_get_contents(extensionPath + '/devices.json');
+    var [, extensionPath,] = getCurrentFile();
+    var [ok, contents] = GLib.file_get_contents(extensionPath + '/devices.json');
     if (ok) {
         devDescriptions = JSON.parse(contents);
     } else {
@@ -76,34 +76,57 @@ function startDaemon() {
     GLib.spawn_async(null, ["bash", "-c", "adb devices"], null, GLib.SpawnFlags.SEARCH_PATH, null, null);
 }
 
-function getModel() {
-    let cmd = 'adb shell getprop ro.product.model';
-    let [res, out, error, status] = GLib.spawn_sync(null, ["bash", "-c", cmd], null, GLib.SpawnFlags.SEARCH_PATH, null);
+function getConnectedDevices() {
+    var devices = [];
+    var [res, out, error, status] = GLib.spawn_sync(null, ["bash", "-c", "adb devices"], null, GLib.SpawnFlags.SEARCH_PATH, null);
+    if (status !== 0) {
+        return devices;
+    }
+    var lines = out.toString().split("\n");
+    if (lines.length < 2) {
+        return devices;
+    }
+    for (var i = 1; i < lines.length; i++) {
+        let parts = lines[i].split("\t");
+        if (parts.length < 2) {
+            continue;
+        }
+        if (parts[1] !== "device") {
+            continue;
+        }
+        devices.push(parts[0]);
+    }
+    return devices;
+}
+
+function getModel(deviceId) {
+    var cmd = 'adb -s ' + deviceId + ' shell getprop ro.product.model';
+    var [res, out, error, status] = GLib.spawn_sync(null, ["bash", "-c", cmd], null, GLib.SpawnFlags.SEARCH_PATH, null);
     return out.toString().replace("\n", "");
 }
 
 function getDevDescription(model) {
-    let ret = model;
+    var ret = model;
     if (ret in devDescriptions) {
-      let brand = devDescriptions[ret]["brand"];
-      let name = devDescriptions[ret]["name"];
-      ret = brand + ((name !== "") ? " " + name : "");
+        let brand = devDescriptions[ret]["brand"];
+        let name = devDescriptions[ret]["name"];
+        ret = brand + ((name !== "") ? " " + name : "");
     }
     return (ret !== "") ? ret : model;
 }
 
-function getChargeInfo() {
-    let cmd = 'adb shell dumpsys battery';
-    let [res, out, error, status] = GLib.spawn_sync(null, ["bash", "-c", cmd], null, GLib.SpawnFlags.SEARCH_PATH, null);
+function getChargeInfo(deviceId) {
+    var cmd = 'adb -s ' + deviceId + ' shell dumpsys battery';
+    var [res, out, error, status] = GLib.spawn_sync(null, ["bash", "-c", cmd], null, GLib.SpawnFlags.SEARCH_PATH, null);
     if (status !== 0) {
         return "";
     }
-    let result = txtToMap(out.toString());
+    var result = txtToMap(out.toString());
     if (result.size == 0) {
         return "";
     }
-    let currTimestamp = Math.floor(Date.now() / 1000);
-    let currLevel = result.has("level") ? result.get("level") : -1;
+    var currTimestamp = Math.floor(Date.now() / 1000);
+    var currLevel = result.has("level") ? result.get("level") : -1;
     if (beginBatteryLevel == -1) {
         beginBatteryLevel = currLevel;
     }
@@ -125,11 +148,11 @@ function getChargeInfo() {
             prevBatteryLevel = currLevel;
         }
     }
-    let message = ((currLevel > -1) ? "Battery " + currLevel + "%" : "Getting battery info error") + lastEstimation;
+    var message = ((currLevel > -1) ? "Battery " + currLevel + "%" : "Getting battery info error") + lastEstimation;
     if (currLevel == 100) {
         message = "Battery fully charged";
     }
-    return message + " (" + getDevDescription(getModel()) + ")";
+    return message + " (" + getDevDescription(getModel(deviceId)) + ")";
 }
 
 function showInfo() {
@@ -155,12 +178,15 @@ function hideInfo() {
 }
 
 function updateBattery() {
-    let info = getChargeInfo()
-    if (info !== "") {
-        panelButtonText.set_text(info);
-        showInfo();
+    var devices = getConnectedDevices();
+    if (devices.length > 0) {
+        let info = getChargeInfo(devices[0]);
+        if (info !== "") {
+            panelButtonText.set_text(info);
+            showInfo();
+        }
     } else {
-        hideInfo()
+        hideInfo();
     }
     return true;
 }
@@ -180,8 +206,8 @@ function txtToMap(str) {
     var params = str.split("\n");
     var dict = new Map();
     for (var i = 0; i < params.length; i++) {
-      var parts = params[i].split(":");
-      dict.set(parts[0].trim(), parts.length > 1 ? parts[1].trim() : "");
+        let parts = params[i].split(":");
+        dict.set(parts[0].trim(), parts.length > 1 ? parts[1].trim() : "");
     }
     return dict
 }
