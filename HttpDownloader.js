8961510e973c69fd007c14512837126b8cedcac5
Головин Алексей Валерьевич <golovin_av@rtsoft.ru>
2022-11-08 08:34:14 +0300

    Devices reference download by Javascript.

---
+++ HttpDownloader.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const {Gio, GLib} = imports.gi;
+imports.gi.versions.Soup = "3.0"; // select version to import
+const Soup = imports.gi.Soup;
+
+export default class HttpDownloader {
+    constructor(loop) {
+        this._loop = loop;
+        this._httpSession = new Soup.Session();
+        this.reset();
+    }
+
+    get charset() {
+        return this._charset;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    get hash() {
+        return this._hash;
+    }
+
+    _loopQuit() {
+        if (this._loop) {
+            this._loop.quit();
+        }
+    }
+
+    reset() {
+        this._request = null;
+        this._charset = '';
+        this._hash = '';
+        this._data = null;
+        this._success = true;
+        this._error = null;
+    }
+
+    splice_callback(outputStream, result) {
+        try {
+            outputStream.splice_finish(result);
+            this._data = outputStream.steal_as_bytes();
+        } catch (err) {
+            this._success = false;
+            this._error = err;
+        }
+        this._loopQuit()
+    }
+
+    send_async_callback(self, res) {
+        let inputStream;
+        try {
+            inputStream = this._httpSession.send_finish(res);
+        } catch (err) {
+            this._success = false;
+            this._error = err;
+            this._loopQuit()
+            return;
+        }
+        if (this._request.status_code == 200) {
+            let response_headers = this._request.get_response_headers();
+            response_headers.foreach((name, value) => {
+                console.log(name, ':', value);
+                if ((name == 'x-goog-hash') && (value.split('=')[0] == 'md5')) {
+                    this._hash = value.split('=')[1] + '==';
+                }
+                if (name == 'Content-Type') {
+                    this._charset = value.split('; ')[1].split('=')[1];
+                }
+            });
+            // charset = response_headers.get_one('Content-Type').split('; ')[1].split('=')[1];
+            let outputStream = Gio.MemoryOutputStream.new_resizable();
+            outputStream.splice_async(
+                inputStream,
+                Gio.OutputStreamSpliceFlags.CLOSE_TARGET,
+                GLib.PRIORITY_DEFAULT,
+                null,
+                this.splice_callback.bind(this),
+            );
+        } else {
+            this._success = false;
+            this._error = this._request;
+            this._loopQuit()
+        }
+    }
+
+    get(url) {
+        this.reset();
+        try {
+            this._request = new Soup.Message({
+                method: 'GET',
+                uri: GLib.Uri.parse(url, GLib.UriFlags.NONE),
+            });
+            this._httpSession.send_async(
+                this._request,
+                null,
+                null,
+                this.send_async_callback.bind(this),
+            );
+        } catch (err) {
+            this._success = false;
+            this._error = err;
+        }
+        if (this._success && this._loop) {
+            this._loop.run();
+        }
+        return new Promise((resolve, reject) => {
+            if (this._success) {
+                resolve(true);
+            } else {
+                reject(this._error);
+            }
+        });
+    }
+}
--- fetch_devices.js
+++ fetch_devices.js
@@ -14,98 +14,50 @@ const Me = ExtensionUtils.getCurrentExtension();
 const CSV = Me.imports.CSV;
 */
 import CSV from './CSV.mjs';
+import HttpDownloader from './HttpDownloader.js';
 
-const loop = GLib.MainLoop.new(null, false);
-const _httpSession = new Soup.Session();
-// Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
-
-var request = new Soup.Message({
-    method: 'GET',
-    uri: GLib.Uri.parse( DEVICES_DB_URL, GLib.UriFlags.NONE),
-});
-var charset;
-var hash;
-
-function splice_callback(outputStream, result) {
-    let data;
-
-    try {
-        outputStream.splice_finish(result);
-        data = outputStream.steal_as_bytes();
-    } catch (err) {
-        logError(err);
-        loop.quit();
-        return;
-    }
-
-    const csvDialect = {
-        quote: '"',
-        separators: ',',
-        ignoreSpacesAfterQuotedString: true,
-        linefeedBeforeEOF: true,
-    };
-    let decoder = new TextDecoder(charset);
-    // let csv_text = decoder.decode(data.toArray());
-    let parsed = CSV.parse(
-        decoder.decode(data.toArray()),
-        csvDialect,
-    );
-    let defReference = {};
-    parsed.mappedRows.forEach(function(row) {
-        defReference[row["Model"]] = {
-            'brand': row["﻿Retail Branding"],
-            'name': row["Marketing Name"],
-            'device': row["Device"]
+var loop = GLib.MainLoop.new(null, false);
+var downloader = new HttpDownloader(loop)
+var doownloadComplete = downloader.get(DEVICES_DB_URL);
+doownloadComplete
+    .then(function(data) {
+        console.log('charset =', downloader.charset);
+        console.log('MD5 hash =', downloader.hash);
+        const csvDialect = {
+            quote: '"',
+            separators: ',',
+            ignoreSpacesAfterQuotedString: true,
+            linefeedBeforeEOF: true,
         };
-    });
-    // Me.path + GLib.DIR_SEPARATOR_S + DEVICES_DB_FILE
-    let fout = Gio.File.new_for_path(DEVICES_DB_FILE);
-    let [ok, etag] = fout.replace_contents(
-        JSON.stringify(defReference, null, 2),
-        null,
-        false,
-        Gio.FileCreateFlags.REPLACE_DESTINATION,
-        null,
-    );
-    GLib.free(etag);
-    loop.quit();
-}
-
-function send_async_callback(self, res) {
-    let inputStream;
-
-    try {
-        inputStream = _httpSession.send_finish(res);
-    } catch (err) {
-        logError(err);
-        loop.quit();
-        return;
-    }
-
-    console.log('status:', request.status_code, request.reason_phrase);
-
-    const response_headers = request.get_response_headers();
-    response_headers.foreach((name, value) => {
-        console.log(name, ':', value);
-        if ((name == 'x-goog-hash') && (value.split('=')[0] == 'md5')) {
-            hash = value.split('=')[1] + '==';
+        let decoder = new TextDecoder(downloader.charset);
+        let parsed = CSV.parse(
+            decoder.decode(downloader.data.toArray()),
+            csvDialect,
+        );
+        let defReference = {};
+        parsed.mappedRows.forEach(function(row) {
+            defReference[row["Model"]] = {
+                'brand': row["﻿Retail Branding"],
+                'name': row["Marketing Name"],
+                'device': row["Device"]
+            };
+        });
+        // Me.path + GLib.DIR_SEPARATOR_S + DEVICES_DB_FILE
+        let fout = Gio.File.new_for_path(DEVICES_DB_FILE);
+        let [ok, etag] = fout.replace_contents(
+            JSON.stringify(defReference, null, 2),
+            null,
+            false,
+            Gio.FileCreateFlags.REPLACE_DESTINATION,
+            null,
+        );
+        GLib.free(etag);
+        console.log('Devices reference updated.')
+    })
+    .catch(function(err) {
+        if (err instanceof Soup.Message) {
+            console.error('"%s" download status: %d %s', DEVICES_DB_URL, err.status_code, err.reason_phrase);
+        } else {
+            console.error(err);
         }
     });
-    const lastModified = response_headers.get_one('Last-Modified');
-    charset = response_headers.get_one('Content-Type').split('; ')[1].split('=')[1];
-    console.log('hash =', hash);
-    
-
-    const outputStream = Gio.MemoryOutputStream.new_resizable();
-    outputStream.splice_async(
-        inputStream,
-        Gio.OutputStreamSpliceFlags.CLOSE_TARGET,
-        GLib.PRIORITY_DEFAULT,
-        null,
-        splice_callback,
-    );
-}
-
-_httpSession.send_async(request, null, null, send_async_callback);
-
-loop.run();
