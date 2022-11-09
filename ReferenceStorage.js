f5a67f6938b80c7b3b0c4b0b16f95764a71e1257
Головин Алексей Валерьевич <golovin_av@rtsoft.ru>
2022-11-09 14:42:18 +0300

    Chaining promises and state machine.

--- ReferenceStorage.js
+++ ReferenceStorage.js
@@ -67,9 +67,13 @@ var ReferenceStorage = class ReferenceStorage {
         );
         this._updateState = 'checkHash';
         this._downloader = new HttpDownloader(null);
-        let actionComplete = this._downloader.head(ReferenceStorage.DEVICES_DB_URL);
-        actionComplete.then(
+        this._downloader.head(
+            ReferenceStorage.DEVICES_DB_URL
+        ).then(
             this._onHashCheck.bind(this),
+            this._onError.bind(this),
+        ).then(
+            this._onFileLoaded.bind(this),
             this._onError.bind(this),
         );
     }
@@ -80,14 +84,11 @@ var ReferenceStorage = class ReferenceStorage {
             console.log(
                 '[ADB-battery-information] Remote resource not changed.',
             );
-            _onLoadRemoteFinalize();
-        } else {
-            let actionComplete = this._downloader.get(ReferenceStorage.DEVICES_DB_URL);
-            actionComplete.then(
-                this._onFileLoaded.bind(this),
-                this._onError.bind(this),
-            );
+            return new Promise(function(resolve, reject) {
+                reject(null);
+            });
         }
+        return this._downloader.get(ReferenceStorage.DEVICES_DB_URL);
     }
 
     _onFileLoaded(data) {
@@ -147,7 +148,9 @@ var ReferenceStorage = class ReferenceStorage {
             GLib.free(etag);
             this._updateState = 'end';
         }
-        _onLoadRemoteFinalize();
+        return new Promise(function(resolve, reject) {
+            reject(null);
+        });
     }
 
     _onError(err) {
@@ -162,6 +165,7 @@ var ReferenceStorage = class ReferenceStorage {
             } else {
                 console.error('[ADB-battery-information] HEAD request error %s', err);
             }
+            this._updateState = 'end';
         }
         if (this._updateState == 'loadFile') {
             if (err instanceof Soup.Message) {
@@ -174,12 +178,8 @@ var ReferenceStorage = class ReferenceStorage {
             } else {
                 console.error('[ADB-battery-information] GET request error %s', err);
             }
+            this._updateState = 'end';
         }
-        this._updateState = 'end';
-        _onLoadRemoteFinalize();
-    }
-
-    _onLoadRemoteFinalize() {
         this._downloader = null;
         console.log(
             '[ADB-battery-information] Devices reference update complete.',
